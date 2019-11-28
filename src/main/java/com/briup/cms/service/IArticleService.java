5feba24fdd8fd51ce2e75d00af366313cbea50a6
youhy <490823791@qq.com>
2019-11-28 09:38:55 +0800

    补充文章插入功能

--- src/main/java/com/briup/cms/service/IArticleService.java
+++ src/main/java/com/briup/cms/service/IArticleService.java
@@ -3,7 +3,6 @@ package com.briup.cms.service;
 import com.briup.cms.bean.Article;
 import org.springframework.stereotype.Service;
 
-@Service
 public interface IArticleService {
     void addArticle(Article article);
 }
